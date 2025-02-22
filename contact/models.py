88d33a859934fc2a5774e3fb241ac7db0799f707
Matheus <matheus.brgsz@gmail.com>
2025-02-22 15:17:43 -0300

    Add initial migration and model for Contact

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Contatc(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=250, blank=True)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self) -> str:
+        return  self.first_name
