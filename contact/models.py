510b22ad0e7210474b608861ff603a551a7d3ab3
Matheus <matheus.brgsz@gmail.com>
2025-02-22 15:24:35 -0300

    Rename model 'Contatc' to 'Contact' and register in admin

--- contact/models.py
+++ contact/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-class Contatc(models.Model):
+class Contact(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50, blank=True)
     phone = models.CharField(max_length=50)
