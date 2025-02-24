85a42ff0ba2e63988c4e39d9a535e73cdeb90427
Matheus <matheus.brgsz@gmail.com>
2025-02-24 10:35:09 -0300

    Implement responsive contact table and update index view to fetch visible contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-
+from contact.models import Contact
 
 def index(request):
+
+    contacts = Contact.objects.filter(show=True).order_by("-id")[:10]
+    
+    print(contacts.query)
+
     return render(
         request, 
-        "contact/index.html"
+        "contact/index.html",
+        {"contacts" : contacts}
         )
