5620f534f72c33af2205b44715fcd5eebd2055d3
Matheus <matheus.brgsz@gmail.com>
2025-02-24 10:35:35 -0300

    Update ContactAdmin to include 'show' field in list display and make it editable

--- contact/admin.py
+++ contact/admin.py
@@ -5,13 +5,13 @@ from contact import models
 @admin.register(models.Contact)
 class ContaticAdmin(admin.ModelAdmin):
     
-    list_display = ('id','first_name', 'last_name', 'phone')
+    list_display = ('id','first_name', 'last_name', 'phone', "show")
     ordering = ('-id',)
     search_fields = ('id', 'first_name', 'last_name')
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = "phone",
-    list_display_links = 'id', 'first_name',
+    list_editable = ("phone", "show")
+    list_display_links = ('id', 'first_name')
 
 
 @admin.register(models.Category)
